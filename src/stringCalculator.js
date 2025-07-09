921110a8dac54a9c398a08def443fc60d6169cb1
hiren-ramoliya <hiren.ramoliya367@gmail.com>
2025-07-09 11:17:00 +0530

    feat: support newline delimiter

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,6 +1,7 @@
 function add(numbers) {
   if (!numbers) return 0;
 
+  numbers = numbers.replace(/\n/g, ",");
   const numArray = numbers.split(",").map(Number);
   return numArray.reduce((sum, num) => sum + num, 0);
 }
