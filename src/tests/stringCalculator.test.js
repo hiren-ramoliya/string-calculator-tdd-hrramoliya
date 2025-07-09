27a7e12b3660c02b53603cab6814670abc26f0d9
hiren-ramoliya <hiren.ramoliya367@gmail.com>
2025-07-09 11:08:37 +0530

    test: return value for single number

--- src/tests/stringCalculator.test.js
+++ src/tests/stringCalculator.test.js
@@ -3,3 +3,7 @@ const { add } = require("../stringCalculator");
 test("return 0 for empty string", () => {
   expect(add("")).toBe(0);
 });
+
+test("return value for single number", () => {
+  expect(add("7")).toBe(7);
+});
