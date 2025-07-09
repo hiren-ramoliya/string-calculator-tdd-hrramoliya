f04ff9465519e88ae291dc07ef8cfef5e6a0048c
hiren-ramoliya <hiren.ramoliya367@gmail.com>
2025-07-09 11:26:46 +0530

    feat: support custom delimiter parsing

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -1,8 +1,15 @@
 function add(numbers) {
   if (!numbers) return 0;
 
-  numbers = numbers.replace(/\n/g, ",");
-  const numArray = numbers.split(",").map(Number);
+  let delimiter = ",";
+  if (numbers.startsWith("//")) {
+    const parts = numbers.split("\n");
+    delimiter = parts[0].substring(2);
+    numbers = parts.slice(1).join("\n");
+  }
+
+  numbers = numbers.replace(/\n/g, delimiter);
+  const numArray = numbers.split(delimiter).map(Number);
   return numArray.reduce((sum, num) => sum + num, 0);
 }
 
